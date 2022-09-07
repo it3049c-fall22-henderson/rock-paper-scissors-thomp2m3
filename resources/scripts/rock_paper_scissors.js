049b4c48abc013b2f280ce4417b841343d074b86
thomp2m3 <111824670+thomp2m3@users.noreply.github.com>
2022-09-06 21:58:25 -0400

    index.html and rock_paper_scissors update
    Syntax error on index.html
    Added game logic.

--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -14,9 +14,10 @@ class RockPaperScissors {
    */
   generateCPUResponse(){
     const acceptedValues = [ `rock`, `paper`, `scissors` ];
-
-    return ;
+    let randomPull = Math.floor(Math.random()*3)
+    return acceptedValues[randomPull];
   }
+
   /**
    * returns one of the following values: `win`, `lose`, `tie`
    * tie:
@@ -33,7 +34,21 @@ class RockPaperScissors {
    * @param {string} cpuSelection computer selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   determineWinner(userSelection, cpuSelection){
-
+    const acceptedResults = [ `win`, `lose`, `tie` ];
+    let result;
+    if (userSelection === cpuSelection)
+      result = acceptedResults[2];
+    
+    if (userSelection == 'rock' && cpuSelection == 'scissors') {
+      result = acceptedResults[0];
+    } else if (userSelection == 'paper' && cpuSelection == 'rock') {
+      result = acceptedResults[0];
+    } else if (userSelection == 'scissors' && cpuSelection == 'paper') {
+      result = acceptedResults[0];
+    } else {
+      result = acceptedResults[1];
+    }
+    return result;
   }
 
   /**
@@ -41,7 +56,28 @@ class RockPaperScissors {
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   play(userSelection){
+    let cpuSelection = this.generateCPUResponse();
+    let winner = this.determineWinner(userSelection, cpuSelection);
 
+    //if the user wins the round w log entry
+    if (winner == 'win') {
+      this.score.user++;
+      winner = this.username;
+      this.gameHistoryLog.push(this.username + ' selected ' + userSelection + ', CPU selected ' + cpuSelection + ': ' + winner + ' wins!');
+    }
+    
+    //if the CPU wins the round w log entry
+    if (winner == 'lose') {
+      this.score.cpu++;
+      winner = 'CPU';
+      this.gameHistoryLog.push(this.username + ' selected ' + userSelection + ', CPU selected ' + cpuSelection + ': CPU wins!');
+    }
+
+    //adding log entry for tie
+    if (winner == 'tie') {
+      this.gameHistoryLog.push(this.username + ' selected ' + userSelection + ', CPU selected ' + cpuSelection + ': it is a tie!');
+    }
+    
   }
 
 }
